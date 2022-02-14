6ec150ac66e11beb4c4fe97ea51fb72337f5215d
imamovv <78980839+imamovv@users.noreply.github.com>
2022-02-14 16:05:55 +0300

    4
    1

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\User;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class HomeController extends Controller
 {
@@ -25,11 +26,13 @@ class HomeController extends Controller
      */
     public function index()
     {
-       
+        $goods = DB::connection('mysql1')->select('select * from prices');
+        dd($goods);
         $user =Auth::user();
         $roles = $user->roles()->get();
         //dd($roles);
         return view('home')->with('user', $user)->with('roles', $roles);
+        return view('home',['goods'=>$goods]);
     }
 
     
